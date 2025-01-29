32bd5310b6005952e0cb5ae6ce30ff70cf7d660e
cheshireca7 <34421496+cheshireca7@users.noreply.github.com>
2025-01-29 16:08:04 +0100

    Added -f flag

--- portscan.sh
+++ portscan.sh
@@ -4,18 +4,17 @@ trap k SIGINT
 
 k(){ echo -e "[\033[31mE\033[0m] Oh geez..."; exit 1; }
 d(){ [[ $1 =~ ${pr} ]] && echo $((RANDOM%(${1#*-}-${1%-*}+1)+${1%-*})) || echo "$1"; }; export -f d 
-o(){ if ${o}; then [[ $1 == *"open" ]] && (echo "$1" | tee -a "${e}"); else (echo "$1" | tee -a "${e}"); fi; }
+o(){ if ${o}; then [[ $1 == *"open" ]] && (echo "$1" | tee -a "${f}"); else (echo "$1" | tee -a "${f}"); fi; }
 s(){ echo "${b[@]}" | (sed 's/ /\n/g'||true) | (shuf||true) | (xargs -P"${t}" -I{} bash -c $'timeout $2 bash -c \'</dev/tcp/{}\' &>/dev/null && (echo \'{} open\' | sed \'s/\//:/\') || (echo \'{} closed\' | sed \'s/\//:/\'); sleep $(d $1)' _ "${d}" "${w}"||true) | while read -r l; do o "${l}"; done; }
 v(){ if [[ $1 =~ ${ir} ]]; then return 0; elif host -T "$1" "${r}" &>/dev/null; then return 0; else echo -e "[\033[31mE\033[0m] Failed to resolve $1"; return 1; fi; }
 l(){ for p in $(seq "$2" "$3"); do b+=("$1\/${p}"); done; }
 p(){ IFS=',' read -ra ps <<< "$1"; for h in $(printf "%s\n" "${ips[@]}"); do v "${h}" && for p in "${ps[@]}"; do if [[ ${p} =~ ${pr} ]]; then l "${h}" "${p%-*}" "${p##*-}"; else l "${h}" "${p}" "${p}"; fi; done; done; s; }
 c(){ IFS=/ read -r i m<<<"$1";IFS=. read -r a b c d<<<"${i}";i=$((a*256**3+b*256**2+c*256+d));s=$((32-m));n=$((2**s));b=$((i&~(n-1)));e=$((b+n-1));for((j=b+1;j<=e-1;j++)); do echo "$((j>>24&255)).$((j>>16&255)).$((j>>8&255)).$((j&255))"; done; }
-u(){ echo -e "~ Bash Port Scanner\nUsage: $0 [-d delay] [-r resolver] [-t threads] [-w <timeout>] [-o open] <ports>\nExample: echo scanme.nmap.org | $0 -d 5-10 20-25,80,443,8000-8008,445" && exit 1; }
-while getopts "d:t:r:w:ho" opt; do case ${opt} in d) d=${OPTARG} ;; r) r=${OPTARG} ;; t) t=${OPTARG} ;; w) w=${OPTARG} ;; o) o=true;; h) u ;; *) u;; esac; done; shift $((OPTIND -1))
+u(){ echo -e "~ Bash Port Scanner\nUsage: $0 [-d <delay>] [-r <resolver>] [-t <threads>] [-w <timeout>] [-o show_open] [-f <output_file>] <ports>\nExample: echo scanme.nmap.org | $0 -d 5-10 20-25,80,443,8000-8008,445" && exit 1; }
+while getopts "d:t:r:w:f:ho" opt; do case ${opt} in d) d=${OPTARG} ;; r) r=${OPTARG} ;; t) t=${OPTARG} ;; f) f=${OPTARG} ;; w) w=${OPTARG} ;; o) o=true;; h) u ;; *) u;; esac; done; shift $((OPTIND -1))
+: "${d:=1}" "${t:=1}" "${w:=1}" "${o:=false}" "${f:=$(date "+%d%m%Y_%H%M%S")_portscan_results.txt}"
 
-: "${d:=1}" "${t:=1}" "${w:=1}" "${o:=false}"
-b=(); a=$1; e="${PWD}/$(date "+%d%m%Y_%H%M%S")_portscan_results.txt"
-ips=$(timeout 1 cat -)
+b=(); a=$1; ips=$(timeout 1 cat -)
 nr='^[0-9]{1,}$'
 pr='^[0-9]{1,5}-[0-9]{1,5}$'
 ar='^[0-9]{1,5}(,[0-9]{1,5}|(-[0-9]{1,5}))?*$'
@@ -26,5 +25,5 @@ dp='7-9,13,21-26,37,53,79,80,81,88,100,106,110-113,119,135,139,143,144,179,199,3
 [[ -z ${ips} ]] && echo -e "\n[\033[34mI\033[0m] Input AWOL!" && exit 1 
 [[ -z ${r} ]] && echo -e "[\033[1;33mW\033[0m] No DNS specified. System DNS will be used!"
 [[ ${ips} =~ ${cr} ]] && echo -e "[\033[34mI\033[0m] Expanding CIDR..." && ips=$(c "${ips}")
-echo -ne "[\033[34mI\033[0m] Scan started"; [[ -z ${a} ]] && echo ". Scanning commom ports..." && a="${dp}"; if [[ ${a} =~ ${ar} ]];then echo && p "${a}"; else u; fi
-echo -ne "\n[\033[34mI\033[0m] Scan finished"; if [[ -s ${e} ]]; then sort -V "${e}" -o "${e}" && echo -e ". Results stored in ${e}"; else rm -f "${e}" &>/dev/null && [[ $o ]] && echo ". No open ports"; fi;
+echo -ne "[\033[34mI\033[0m] Scan started"; [[ -z ${a} ]] && echo -n ". Scanning commom ports..." && a="${dp}"; if [[ ${a} =~ ${ar} ]];then echo && p "${a}"; else u; fi
+echo -ne "\n[\033[34mI\033[0m] Scan finished"; if [[ -s ${f} ]]; then sort -V "${f}" -o "${f}" && echo -e ". Results stored in ${f}"; else rm -f "${f}" &>/dev/null && ${o} && echo ". No open ports"; fi;
